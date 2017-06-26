d3ac623bdf9d1aac062b6caa238444bf09bcd8a6
bethek <kreshnikiberdemaj@gmail.com>
2017-06-26 12:33:22 +0200

    first push data

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>test</title>
+    </head>
+    <body>
+
+        <form class="item-add" action="app/init.php" method="POST">
+        <input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
+        <input type="submit" value="Add" class="submit">
+        </form>
+
+    </body>
+</html>
